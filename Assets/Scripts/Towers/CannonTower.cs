1685905ccbf4da66a0c382aa3237cd49d5cb67fa
d.rakhmatullin <d.rakhmatullin@corp.nekki.ru>
2018-05-04 18:37:13 +0300

    Towers ref.

---
+++ Assets/Scripts/Towers/CannonTower.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CannonTower : Tower
+{
+    [SerializeField] private Transform _mShootPoint;
+
+    private float _mLastShotTime = -0.5f;
+
+    protected override bool MissingPrefabs()
+    {
+        return base.MissingPrefabs() || _mShootPoint == null;
+    }
+
+    protected override Vector3 GetShootPosition()
+    {
+        return _mShootPoint.position;
+    }
+
+    protected override Quaternion GetShootRotation()
+    {
+        return _mShootPoint.rotation;
+    }
+}
